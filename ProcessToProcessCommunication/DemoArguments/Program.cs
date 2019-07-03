19595685e8f3596a3bbc4cca40984cbf692dc9f8
M Rumman Hasan Khan <mrummanhasan@gmail.com>
2019-07-03 18:02:25 +0500

    Add files via upload

---
+++ ProcessToProcessCommunication/DemoArguments/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoArguments
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length >= 2)
+            {
+                Console.WriteLine(args[0] + "-" + args[1]);
+              
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ProcessToProcessCommunication/DemoConsumer/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoConsumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProcessStartInfo ps = new ProcessStartInfo();
+            ps.FileName = @"C:\Users\glab.KIETNN\Documents\Visual Studio 2012\Projects\DemoArguments\bin\Debug";
+            ps.Arguments = "Hello World";
+
+            Process p = Process.Start(ps);
+            p.StartInfo.RedirectStandardOutput = true;
+            string output = p.StandardOutput.ReadToEnd();
+            Console.WriteLine(output);
+            //p.WaitForExit();
+
+        }
+    }
+}
