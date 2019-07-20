122d0986c88d4216487e4e668b534e349502f1fe
M Rumman Hasan Khan <mrummanhasan@gmail.com>
2019-07-20 14:26:24 +0500

    Add files via upload

---
+++ interFileExecution/Main.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+`gcc -o file1 file1.c`
+echo `./file1`
+
+`chmod +x file2.sh`
+echo `./file2.sh`
+
+`xdg-open file3.txt`
---
+++ interFileExecution/file1.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main ()
+{
+ printf("This is C-File1");
+ return 0;
+}
