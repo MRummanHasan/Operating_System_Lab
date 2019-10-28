08f4d2aca459ccc2404250f02516fb93004acd25
MRayyanHKhan <33967907+MRayyanHKhan@users.noreply.github.com>
2019-10-28 23:36:55 +0500

    Update fork.c

--- C_lab/fork.c
+++ C_lab/fork.c
@@ -5,8 +5,28 @@
 int main()
 {
 
- int pid = fork();
- printf("Process ID: %d \n", pid);
- 
-return 0;
+	pid_t pid = fork();
+	printf("Process ID: %d \n", pid);
+
+	if(pid < 0)
+	{
+		printf("Unsuccessful child process creation.\n");
+		exit(0);
+	}
+	else if(pid > 0)
+	{
+		wait();
+		for(int i=0; i <= 10 ; i+=2)
+			printf("%d\n", i);
+		printf("Parent Ends.");
+	}
+	else
+	{
+		getpid();
+		for(int i=1; i <= 10 ; i+=2)
+			printf("%d\n", i);
+		printf("Child Ends.");
+	}
+
+	return 0;
 }
