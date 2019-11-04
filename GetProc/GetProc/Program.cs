f4de8e1e908a0f414f1f0045291263f02167c064
M Rumman Hasan Khan <mrummanhasan@gmail.com>
2019-11-04 11:27:04 +0500

    show running process and threads

---
+++ GetProc/GetProc/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetProc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Task Manager");
+            Console.WriteLine();
+            Console.WriteLine("1. Show current process");
+            Console.WriteLine("2. List all the processes");
+            
+
+            string userinput = Console.ReadLine();
+            if (userinput == "1")
+            {
+                getProcess();
+            }
+            else if (userinput == "2")
+            {
+                getListofProcesses();    
+            }
+            Console.ReadLine();
+        }
+
+
+        public static void getProcess()
+        {
+            Process thisProc = Process.GetCurrentProcess();
+            string procName = thisProc.ProcessName;
+            DateTime started = thisProc.StartTime;
+            int procID = thisProc.Id;
+            int memory = thisProc.VirtualMemorySize;
+            int priMemory = thisProc.PrivateMemorySize;
+            int physMemory = thisProc.WorkingSet;
+            int priority = thisProc.BasePriority;
+            ProcessPriorityClass priClass = thisProc.PriorityClass;
+            TimeSpan cpuTime = thisProc.TotalProcessorTime;
+            Console.WriteLine("Process: {0}, ID: {1}", procName, procID);
+            Console.WriteLine(" started: {0}", started.ToString());
+            Console.WriteLine(" CPU time: {0}", cpuTime.ToString());
+            Console.WriteLine(
+            " priority class: {0} priority: {1}", priClass, priority);
+            Console.WriteLine(" virtual memory: {0}", memory);
+            Console.WriteLine(" private memory: {0}", priMemory);
+            Console.WriteLine(" physical memory: {0}", physMemory);
+            Console.WriteLine("\n trying to change priority...");
+            thisProc.PriorityClass = ProcessPriorityClass.High;
+            priClass = thisProc.PriorityClass;
+            Console.WriteLine(" new priority class: {0}", priClass);
+
+
+        }
+
+        public static void getListofProcesses()
+        {
+            int totMemory = 0;
+            Console.WriteLine("Info for all processes:");
+            Process[] allProcs = Process.GetProcesses();
+            foreach (Process thisProc in allProcs)
+            {
+                string procName = thisProc.ProcessName;
+                //DateTime started = thisProc.StartTime;
+                int procID = thisProc.Id;
+                int memory = thisProc.VirtualMemorySize;
+                int priMemory = thisProc.PrivateMemorySize;
+                int physMemory = thisProc.WorkingSet;
+                totMemory += physMemory;
+                int priority = thisProc.BasePriority;
+                ////TimeSpan cpuTime = thisProc.TotalProcessorTime;
+                Console.WriteLine("Process: {0}, ID: {1}", procName, procID);
+                //Console.WriteLine(" started: {0}", started.ToString());
+                //Console.WriteLine(" CPU time: {0}", cpuTime.ToString());
+                Console.WriteLine(" virtual memory: {0}", memory);
+                Console.WriteLine(" private memory: {0}", priMemory);
+                Console.WriteLine(" physical memory: {0}", physMemory);
+            }
+            Console.WriteLine("\nTotal physical memory used: {0}", totMemory);
+        }
+
+    }
+}
---
+++ GetThreads/GetThreads/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetThreads
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Thread Manager");
+            Console.WriteLine();
+            Console.WriteLine("1. Show current threads");
+            Console.WriteLine("2. List all the threads");
+
+
+            string userinput = Console.ReadLine();
+            if (userinput == "1")
+            {
+                getThreads();
+            }
+            else if (userinput == "2")
+            {
+                getListofThreads();
+            }
+            Console.ReadLine();
+        }
+
+        public static void getThreads()
+        {
+
+            Process thisProc = Process.GetCurrentProcess();
+            ProcessThreadCollection myThreads = thisProc.Threads;
+            foreach (ProcessThread pt in myThreads)
+            {
+                DateTime startTime = pt.StartTime;
+                TimeSpan cpuTime = pt.TotalProcessorTime;
+                int priority = pt.BasePriority;
+                ThreadState ts = pt.ThreadState;
+                Console.WriteLine("thread: {0}", pt.Id);
+                Console.WriteLine(" started: {0}", startTime.ToString());
+                Console.WriteLine(" CPU time: {0}", cpuTime);
+                Console.WriteLine(" priority: {0}", priority);
+                Console.WriteLine(" thread state: {0}", ts.ToString());
+            }
+        }
+        
+        public static void getListofThreads()
+        {
+            Process[] allProcs = Process.GetProcesses();
+            foreach (Process proc in allProcs)
+            {
+                ProcessThreadCollection myThreads = proc.Threads;
+                Console.WriteLine("process: {0}, id: {1}", proc.ProcessName, proc.Id);
+                foreach (ProcessThread pt in myThreads)
+                {
+                    //DateTime startTime = pt.StartTime;
+                    //TimeSpan cpuTime = pt.TotalProcessorTime;
+                    int priority = pt.BasePriority;
+                    ThreadState ts = pt.ThreadState;
+
+                    Console.WriteLine(" thread: {0}", pt.Id);
+                    //Console.WriteLine(" started: {0}", startTime.ToString());
+                    //Console.WriteLine(" CPU time: {0}", cpuTime);
+                    Console.WriteLine(" priority: {0}", priority);
+                    Console.WriteLine(" thread state: {0}", ts.ToString());
+                }
+            }
+        }
+    }
+}
